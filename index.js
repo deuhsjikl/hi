52f9601f893898675e4270d35a1a78002395bce6
deuhsjikl <msjcomentaneo56@hotmail.com>
2024-05-13 19:03:35 +0700

    commit

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-function isValidBST(root) {
-  let prev = null;
-  return inorder(root);
-  function inorder(node) {
-    if (!node) return true;
-    if (!inorder(node.left)) return false;
-    if (prev !== null && node.val <= prev) return false;
-    prev = node.val;
-    return inorder(node.right);
+const linearSearch = (arr, target) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
   }
-}
+  return -1;
+};
